d67f70040259452804fbea06b229c7a1ae2da388
Hu, Ming <huming@inc.cuhk.edu.hk>
2013-05-07 14:27:57 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vote::Application.config.secret_token = 'd2a4dbb980c83ccf80ad3b433a06567092dacc3cb6c52d5236afc7a4226610a07d5ce7842e7b9f2068f95d507ab3e695f401bddc2493a9fcd55853227479edb2'
