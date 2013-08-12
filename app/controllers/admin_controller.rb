c9c32f49d698c4b51b452c1eae488d99c959b83a
Hu, Ming <ming.kernel@gmail.com>
2013-08-12 12:09:16 +0800

    do not delete admin.stop

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -71,7 +71,7 @@ class AdminController < ApplicationController
     User.delete_all("name != 'admin'")
     Group.delete_all()
     Proposal.delete_all()
-    Admin.delete_all()
+    # Admin.delete_all()
     redirect_to admin_url, :notice => 'All users, groups and proposals deleted'
   end
 
